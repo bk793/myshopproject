61f9fd5595bc0a665ad5d2dda267bdd74ba0e4e0
BrajKishor467 <srivastavabrajkishor@gmail.com>
2025-08-30 12:02:28 +0530

    adding missed file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withFetch} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withFetch()), // ✅ Enables HTTP requests
+    provideHttpClient(), // ✅ Enables HTTP requests
     provideRouter(routes) // ✅ Enables routing for navigation
   ]
 };
